d58b3761fec99e236856022fecfc5ec454c5f2da
Dan Batchelder <danielbatchelder@icloud.com>
2022-08-08 12:15:30 -0400

    In DB: updater user table, added restrictions and user_restrictions tables

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -1,6 +1,11 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (user_email,first_name,last_name,password_hash) VALUES ('user1@hotmail.com', 'User', 'One', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC');
+INSERT INTO users (user_email,first_name,last_name,password_hash) VALUES ('user2@gmail.org', 'User', 'Two', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC');
+
+
+INSERT INTO restrictions (restriction_name) VALUES ('Dairy Free'),('Gluten Free'),('Halal'),('Kosher'),('Low-carb'),('Nut Free'),('Vegan'),('Vegetarian');
+
+INSERT INTO user_restrictions (user_id, restriction_id) VALUES (1001, 2001), (1001, 2002), (1001, 2008);
 
 COMMIT TRANSACTION;

0113005dbcb55ef0967d3c78303eee980ff7de9f
Dan Batchelder <danielbatchelder@icloud.com>
2022-08-08 15:09:46 -0400

    Expanded global CSS

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -2,8 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
     </div>
     <router-view />
+    <footer>
+      ©2022 Potluck Planner | Tech Elevator Capstone | Dan Batchelder - Robyn
+      Florentz - Conor Smith - Robert Weeks
+    </footer>
   </div>
 </template>

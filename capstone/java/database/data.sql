a5b984696e2c03ed71157f7267bf1a0120320be8
Dan Batchelder <danielbatchelder@icloud.com>
2022-08-10 13:19:28 -0400

    Added constraint to user role types

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (user_email,first_name,last_name,password_hash,role) VALUES ('user1@hotmail.com', 'User', 'One', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','admin');
-INSERT INTO users (user_email,first_name,last_name,password_hash,role) VALUES ('user2@gmail.org', 'User', 'Two', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','user');
+INSERT INTO users (user_email,first_name,last_name,password_hash,role) VALUES ('user1@hotmail.com', 'User', 'One', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (user_email,first_name,last_name,password_hash,role) VALUES ('user2@gmail.org', 'User', 'Two', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 
 
 INSERT INTO restrictions (restriction_abbrev, restriction_name) VALUES ('df', 'Dairy Free'),('gf', 'Gluten Free'),('h', 'Halal'),('k', 'Kosher'),('lc', 'Low-carb'),('nf', 'Nut Free'),('vn', 'Vegan'),('v', 'Vegetarian');

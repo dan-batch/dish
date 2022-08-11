7eee3b920d05cae10c955a7cacb133836d132b3f
Robert Weeks <rpweeks_2@yahoo.com>
2022-08-11 16:37:37 -0400

    committing updates to home page

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
-    <!-- Router Link is for Demo -->
-    <router-link to="/myProfile">
-      Click here to view your profile.
-    </router-link>
+    <body>
+      <div class="header-img">
+        <img
+          src="../assets/Dish_Logo_Cropped.png"
+          alt="dish logo"
+          class="logo"
+        />
+      </div>
+
+      <!-- Router Link is for Demo -->
+      <router-link to="/myProfile" class="well">
+        View & edit your user profile
+      </router-link>
+      <br />
+      <router-link to="/events" class="well"
+        >Plan your next potluck event</router-link
+      >
+      <br />
+      <router-link to="/upcoming_events" class="well"
+        >View upcoming potluck events</router-link
+      >
+      <br />
+      <router-link to="/past_events" class="well"
+        >View past potluck events</router-link
+      >
+    </body>
   </div>
 </template>
 
@@ -14,3 +34,38 @@ export default {
   name: "home",
 };
 </script>
+
+<style scoped>
+.logo {
+  max-width: 150px;
+}
+
+.header-img {
+  grid-area: ga-logo;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+}
+
+.well {
+  background-color: white;
+  border-radius: 15px;
+  width: 250px;
+  height: 50px;
+  margin: 5px;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  font-weight: 900;
+}
+
+body {
+  background-image: url("../assets/Home_Page_Background.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  height: 100hv;
+}
+</style>

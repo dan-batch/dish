1cc4587ba7fd059ff6fd5359e14fe7e58ecb8654
Dan Batchelder <danielbatchelder@icloud.com>
2022-08-16 14:02:22 -0400

    Added more to dish service yay

--- capstone/vue/src/services/DishService.js
+++ capstone/vue/src/services/DishService.js
@@ -5,7 +5,19 @@ export default {
         return axios.get(`/dish/dish${id}`)
     },
 
+    addDish() {
+        return axios.post(`/dish/create`)
+    },
+
     updateDish(id, updatedDish) {
         return axios.put(`/dish/dish${id}`, updatedDish);
     },
+
+    getDishByPluck(pluckId) {
+        return axios.get(`/dish/pluck${pluckId}`)
+    },
+
+    deleteDish(id) {
+        return axios.delete(`/dish/${id}`);
+    }
 }
